8de8feb669e86d944b15a8d6841a4509400c9496
JoeVincentt <eugene.bb@hotmail.com>
2018-11-21 22:54:39 -0500

    Update 2

---
+++ src/components/Contacts.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "Karen Williams",
+        email: "Karen@gmail.com",
+        phone: "333-333-3333"
+      },
+      {
+        id: 3,
+        name: "Henry Johnson",
+        email: "Henry@gmail.com",
+        phone: "666-666-6666"
+      }
+    ]
+  };
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      <React.Fragment>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contacts;
