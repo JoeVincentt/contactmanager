af16e6968c935ffb945f0c103de5036caf002fc1
JoeVincentt <eugene.bb@hotmail.com>
2018-11-22 21:05:12 -0500

    Updates

---
+++ precache-manifest.a55289b0a924619aaae0f29f001abf33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "baf045ab594acb58e56a",
+    "url": "/contactmanager/static/js/main.baf045ab.chunk.js"
+  },
+  {
+    "revision": "5e380a8d409926e536c1",
+    "url": "/contactmanager/static/js/1.5e380a8d.chunk.js"
+  },
+  {
+    "revision": "baf045ab594acb58e56a",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "5e380a8d409926e536c1",
+    "url": "/contactmanager/static/css/1.901ff97f.chunk.css"
+  },
+  {
+    "revision": "269fad3d9dbefa8cf7a341f0f8101d8c",
+    "url": "/contactmanager/index.html"
+  }
+];
